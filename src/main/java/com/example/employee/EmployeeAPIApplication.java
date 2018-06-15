cb83ff24c3a116514848679f0526d7f50d2460f3
Angel-kou <1844646307@qq.com>
2018-06-15 17:49:55 +0800

    add annotation

--- src/main/java/com/example/employee/EmployeeAPIApplication.java
+++ src/main/java/com/example/employee/EmployeeAPIApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeAPIApplication {
+	//应用入口
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeAPIApplication.class, args);
 	}
